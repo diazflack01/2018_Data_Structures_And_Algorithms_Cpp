304b454139b5d5a5053dfbc1b62476ef08e303d7
Kelvin Robles <kelvin.robles018@gmail.com>
2018-04-11 20:31:24 +0800

    AVL Tree Delete Implem + UT

--- src/Tree/AvlTree.hpp
+++ src/Tree/AvlTree.hpp
@@ -26,7 +26,7 @@ public:
     // Insertion - https://www.geeksforgeeks.org/avl-tree-set-1-insertion/
     void insert(const T v);
 
-    // Deletion
+    // Deletion - https://www.geeksforgeeks.org/avl-tree-set-2-deletion/
     void deleteNodeWithKey(const T v);
 
     // Traversal
@@ -34,8 +34,10 @@ public:
 
 private:
     AvlNode<T>* insertRecursive(AvlNode<T>* node, const T v);
+    AvlNode<T>* deleteNodeRecursive(AvlNode<T>* node, const T v);
     AvlNode<T>* leftRotate(AvlNode<T>* node);
     AvlNode<T>* rightRotate(AvlNode<T>* node);
+    AvlNode<T>* minValueNode(AvlNode<T>* node);
     int getBalanceFactor(AvlNode<T>* node);
     int height(AvlNode<T>* node);
     AvlNode<T>* newNode(const T v);
@@ -59,13 +61,22 @@ std::vector<T> AvlTree<T>::levelOrderTraversal()
 
         if(nullptr == node)
             continue;
-
+        // std::cout << "key " << node->key << "\t height " << node->height << std::endl;
         ret.push_back(node->key);
         queue.push(node->left);
         queue.push(node->right);
     }
     
     return ret;
+}
+
+template<typename T>
+AvlNode<T>* AvlTree<T>::minValueNode(AvlNode<T>* node)
+{
+    while(nullptr != node->left)
+        node = node->left;
+
+    return node;
 }
 
 template<typename T>
@@ -94,12 +105,6 @@ template<typename T>
 void AvlTree<T>::insert(const T v)
 {
     rootNode = insertRecursive(rootNode, v);
-}
-
-template<typename T>
-void AvlTree<T>::deleteNodeWithKey(const T v)
-{
-
 }
 
 template<typename T>
@@ -174,6 +179,92 @@ AvlNode<T>* AvlTree<T>::rightRotate(AvlNode<T>* node)
     x->height = 1 + std::max(height(x->left), height(x->right));
 
     return x;
+}
+
+template<typename T>
+void AvlTree<T>::deleteNodeWithKey(const T v)
+{
+    rootNode = deleteNodeRecursive(rootNode, v);
+}
+
+template<typename T>
+AvlNode<T>* AvlTree<T>::deleteNodeRecursive(AvlNode<T>* node, const T v)
+{
+    if(nullptr == node)
+        return node;
+
+    if(v < node->key)
+    {
+        node->left = deleteNodeRecursive(node->left, v);
+    }
+    else if(v > node->key)
+    {
+        node->right = deleteNodeRecursive(node->right, v);
+    }
+    else
+    {
+        // perform normal BST node deletion here
+        if(nullptr == node->left || nullptr == node->right)
+        {
+            auto temp = (nullptr == node->left) ? node->right : node->left;
+
+            if(nullptr == temp)
+            {
+                temp = node;
+                node = nullptr;
+            }
+            else if(nullptr == node->left)
+            {
+                node->key = temp->key;
+                node->right = nullptr;
+            }
+            else
+            {
+                node->key = temp->key;
+                node->left = nullptr;
+            }
+
+            delete temp;
+        }
+        else
+        {
+            auto temp = minValueNode(node->right);
+            node->key = temp->key;
+            node->right = deleteNodeRecursive(node->right, temp->key);
+        }
+    }
+
+    if(nullptr == node)
+        return node;
+
+    node->height = 1 + std::max(height(node->left), height(node->right));
+    const auto balanceFactor = getBalanceFactor(node);
+    // std::cout << v << " balanceFactor " << balanceFactor << std::endl;
+
+    if(balanceFactor > 1 && getBalanceFactor(node->left) >= 0) // L, L
+    {
+        std::cout << "right rotating...\n";
+        return rightRotate(node);
+    }
+    else if(balanceFactor > 1 && getBalanceFactor(node->left) < 0) // L, R
+    {
+        std::cout << "left right rotating...\n";
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+    else if(balanceFactor < -1 && getBalanceFactor(node->right) <= 0) // R, R
+    {
+        std::cout << "left rotating...\n";
+        return leftRotate(node);
+    }
+    else if(balanceFactor < -1 && getBalanceFactor(node->right) > 0) // R, L
+    {
+        std::cout << "right left rotating...\n";
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
 }
 
 }; // namespace Tree
--- test/Tree/TestAvlTree.cpp
+++ test/Tree/TestAvlTree.cpp
@@ -128,3 +128,183 @@ TEST(TestAvlTree, insert_mix_fromGeeksForGeeks)
     ASSERT_EQ(25, levelOrderTraversedVector[4]);
     ASSERT_EQ(50, levelOrderTraversedVector[5]);
 }
+
+TEST(TestAvlTree, delete_nodeDoesNotExist)
+{
+    AvlTree_t avlTree;
+
+    avlTree.insert(50);
+    avlTree.insert(60);
+    avlTree.insert(40);
+    avlTree.deleteNodeWithKey(55);
+
+    const auto levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(3, levelOrderTraversedVector.size());
+    ASSERT_EQ(50, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+}
+
+TEST(TestAvlTree, DISABLED_delete_leftLeftUnbalanced_AfterRemoval)
+{
+    AvlTree_t avlTree;
+
+    avlTree.insert(50);
+    avlTree.insert(40);
+    avlTree.insert(60);
+    avlTree.insert(38);
+    avlTree.insert(42);
+
+    auto levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(5, levelOrderTraversedVector.size());
+    ASSERT_EQ(50, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+    ASSERT_EQ(38, levelOrderTraversedVector[3]);
+    ASSERT_EQ(42, levelOrderTraversedVector[4]);
+
+    avlTree.deleteNodeWithKey(42);
+    levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(4, levelOrderTraversedVector.size());
+    ASSERT_EQ(50, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+    ASSERT_EQ(38, levelOrderTraversedVector[3]);
+
+    avlTree.deleteNodeWithKey(60);
+    levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(3, levelOrderTraversedVector.size());
+    ASSERT_EQ(40, levelOrderTraversedVector[0]);
+    ASSERT_EQ(38, levelOrderTraversedVector[1]);
+    ASSERT_EQ(50, levelOrderTraversedVector[2]);
+}
+
+TEST(TestAvlTree, DISABLED_delete_leftRightUnbalanced_AfterRemoval)
+{
+    AvlTree_t avlTree;
+
+    avlTree.insert(50);
+    avlTree.insert(40);
+    avlTree.insert(60);
+    avlTree.insert(42);
+
+    auto levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(4, levelOrderTraversedVector.size());
+    ASSERT_EQ(50, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+    ASSERT_EQ(42, levelOrderTraversedVector[3]);
+
+    avlTree.deleteNodeWithKey(60);
+    levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(3, levelOrderTraversedVector.size());
+    ASSERT_EQ(42, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(50, levelOrderTraversedVector[2]);
+}
+
+TEST(TestAvlTree, DISABLED_delete_rightRightUnbalanced_AfterRemoval)
+{
+    AvlTree_t avlTree;
+
+    avlTree.insert(50);
+    avlTree.insert(40);
+    avlTree.insert(60);
+    avlTree.insert(58);
+    avlTree.insert(62);
+
+    auto levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(5, levelOrderTraversedVector.size());
+    ASSERT_EQ(50, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+    ASSERT_EQ(58, levelOrderTraversedVector[3]);
+    ASSERT_EQ(62, levelOrderTraversedVector[4]);
+
+    avlTree.deleteNodeWithKey(58);
+    levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(4, levelOrderTraversedVector.size());
+    ASSERT_EQ(50, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+    ASSERT_EQ(62, levelOrderTraversedVector[3]);
+
+    avlTree.deleteNodeWithKey(40);
+    levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(3, levelOrderTraversedVector.size());
+    ASSERT_EQ(60, levelOrderTraversedVector[0]);
+    ASSERT_EQ(50, levelOrderTraversedVector[1]);
+    ASSERT_EQ(62, levelOrderTraversedVector[2]);
+}
+
+TEST(TestAvlTree, delete_rightLeftUnbalanced_AfterRemoval)
+{
+    AvlTree_t avlTree;
+
+    avlTree.insert(50);
+    avlTree.insert(40);
+    avlTree.insert(60);
+    avlTree.insert(58);
+    avlTree.insert(62);
+
+    auto levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(5, levelOrderTraversedVector.size());
+    ASSERT_EQ(50, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+    ASSERT_EQ(58, levelOrderTraversedVector[3]);
+    ASSERT_EQ(62, levelOrderTraversedVector[4]);
+
+    avlTree.deleteNodeWithKey(62);
+    levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(4, levelOrderTraversedVector.size());
+    ASSERT_EQ(50, levelOrderTraversedVector[0]);
+    ASSERT_EQ(40, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+    ASSERT_EQ(58, levelOrderTraversedVector[3]);
+
+    avlTree.deleteNodeWithKey(40);
+    levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(3, levelOrderTraversedVector.size());
+    ASSERT_EQ(58, levelOrderTraversedVector[0]);
+    ASSERT_EQ(50, levelOrderTraversedVector[1]);
+    ASSERT_EQ(60, levelOrderTraversedVector[2]);
+}
+
+TEST(TestAvlTree, delete_mix_fromGeeksForGeeks)
+{
+    AvlTree_t avlTree;
+    avlTree.insert(9);
+    avlTree.insert(5);
+    avlTree.insert(10);
+    avlTree.insert(0);
+    avlTree.insert(6);
+    avlTree.insert(11);
+    avlTree.insert(-1);
+    avlTree.insert(1);
+    avlTree.insert(2);
+
+    auto levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(9, levelOrderTraversedVector.size());
+    ASSERT_EQ(9, levelOrderTraversedVector[0]);
+    ASSERT_EQ(1, levelOrderTraversedVector[1]);
+    ASSERT_EQ(10, levelOrderTraversedVector[2]);
+    ASSERT_EQ(0, levelOrderTraversedVector[3]);
+    ASSERT_EQ(5, levelOrderTraversedVector[4]);
+    ASSERT_EQ(11, levelOrderTraversedVector[5]);
+    ASSERT_EQ(-1, levelOrderTraversedVector[6]);
+    ASSERT_EQ(2, levelOrderTraversedVector[7]);
+    ASSERT_EQ(6, levelOrderTraversedVector[8]);
+
+    avlTree.deleteNodeWithKey(10);
+    levelOrderTraversedVector = avlTree.levelOrderTraversal();
+    ASSERT_EQ(8, levelOrderTraversedVector.size());
+    ASSERT_EQ(1, levelOrderTraversedVector[0]);
+    ASSERT_EQ(0, levelOrderTraversedVector[1]);
+    ASSERT_EQ(9, levelOrderTraversedVector[2]);
+    ASSERT_EQ(-1, levelOrderTraversedVector[3]);
+    ASSERT_EQ(5, levelOrderTraversedVector[4]);
+    ASSERT_EQ(11, levelOrderTraversedVector[5]);
+    ASSERT_EQ(2, levelOrderTraversedVector[6]);
+    ASSERT_EQ(6, levelOrderTraversedVector[7]);
+}
